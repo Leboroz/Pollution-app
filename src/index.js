9a094b37514df72f7dbdf92e4ca5193f920af5d9
Leonardo <albornozleo95@gmail.com>
2022-03-07 13:38:59 -0400

    Add Gill Sans font

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './sass/global.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <Provider> */}
+      <App />
+      {/* </Provider> */}
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
