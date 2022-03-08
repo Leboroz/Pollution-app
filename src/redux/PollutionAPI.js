5820675354fcf9ec741807acf24610b8e623d14a
Leonardo <albornozleo95@gmail.com>
2022-03-08 18:04:17 -0400

    Add data to redux

---
+++ src/redux/PollutionAPI.js
@@ -0,0 +1,12 @@
+export default class PollutionAPI {
+  static END_POINT = 'http://api.openweathermap.org/data/2.5/air_pollution';
+  static LAT = '?lat=';
+  static LON = '&lon=';
+  static ID = '&appid=57d14da10d2f873314e79507698059f6';
+
+  static async get(lat, lon) {
+    return fetch(
+      this.END_POINT + this.LAT + lat + this.LON + lon + this.ID
+    ).then((res) => res.json());
+  }
+}
